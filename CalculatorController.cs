f81e7789788e1cbc26c0b305a739b71e5a736ce5
AndreyKrotkov <113906232+AndreyKrotkov@users.noreply.github.com>
2024-05-11 12:07:44 +0300

    Add files via upload

---
+++ CalculatorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new CalculatorModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Add(CalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (int.TryParse(model.Number1, out int number1) && int.TryParse(model.Number2, out int number2))
+                {
+                    model.Result = number1 + number2;
+                    // ViewBag.Result = $"{number1} + {number2} = {model.Result}";
+                    return View("Index", model);
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Недійсні введені дані.";
+                }
+            }
+            return View("Index", model);
+        }
+
+    }
+}
---
+++ CalculatorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class CalculatorModel
+    {
+        public string? Number1 { get; set; }
+        public string? Number2 { get; set; }
+        public int Result { get; set;}
+    }
+}
